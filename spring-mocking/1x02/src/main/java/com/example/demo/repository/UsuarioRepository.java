a567d97e39940e3c7ae3905323aeb6c343f6b1f7
Adilson Vicente dos Santos <adilson_mumu@hotmail.com>
2025-11-13 11:58:44 -0300

    spring

---
+++ spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Usuario;
+
+@Repository
+public interface UsuarioRepository {
+
+    public Optional<Usuario> findById(Long id);
+
+    public Usuario save(Usuario Usuario);
+
+
+}
